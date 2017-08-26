53a8aef2c437bf7e01f533cc14da77fe8576caf6
kozyriev <kozyrev.vitaliy@gmail.com>
2017-08-26 14:42:15 +0300

    dummy database script

---
+++ Database/CreateData.sql
@@ -0,0 +1,26 @@
+USE [University]
+
+INSERT INTO Courses (discipline)
+VALUES ('math'), ('gegraphy'), ('economics'), ('programming')
+
+INSERT INTO Students (name)
+VALUES ('Stephen'), ('Anton'), ('Oleksii')
+
+INSERT INTO Enrollments (course_id, student_id, type)
+SELECT C.course_id, S.strudent_id, 'partial' FROM 
+	(SELECT student_id FROM Students WHERE name = 'Stephen') S
+		CROSS JOIN
+	(SELECT course_id FROM Courses WHERE discipline IN ('math', 'geography', 'economics')) C
+
+INSERT INTO Enrollments (course_id, student_id, type)
+SELECT C.course_id, S.strudent_id, 'full' FROM 
+	(SELECT student_id FROM Students WHERE name = 'Anton') S
+		CROSS JOIN
+	(SELECT course_id FROM Courses WHERE discipline IN ('math', 'programming', 'economics')) C
+
+
+INSERT INTO Enrollments (course_id, student_id, type)
+SELECT C.course_id, S.strudent_id, 'full' FROM 
+	(SELECT student_id FROM Students WHERE name = 'Oleksii') S
+		CROSS JOIN
+	(SELECT course_id FROM Courses WHERE discipline IN ('math', 'programming')) C
